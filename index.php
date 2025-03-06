812c81428880a9b92f437984208c27f3c25f191e
Vitoria Saldanha <saldanhavitoria152@gmail.com>
2025-03-06 20:26:21 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+ <title>Primeira aula</title>
+
+ <head>
+
+ </head>
+
+ <body>
+  <form method = "post"  action = "teste.php">
+    Nome: <input type = "text"  name = "nome"><br>
+    Idade: <input type = "text" name = "idade"><br>
+    <input type = "submit" value="enviar">
+
+
+
+  </form>
+
+
+ </body>
+</html>
---
+++ teste.php
@@ -0,0 +1,7 @@
+<?php
+echo "nome: " .$_POST['nome'];
+echo '<br';
+echo "idade:" .$_POST['idade'];
+echo '<hr>';
+print_r($_POST);
+?>
